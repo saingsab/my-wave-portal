dfc0a85dc3ef5f259d72d8f4976d8419455e083a
Saing Sab <saing.sab@gmail.com>
2021-12-24 11:48:32 +0700

    Store data on our smart contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,25 @@
 
 const main = async () => {
+    const [ownser, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", ownser.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTrx = await waveContract.wave();
+    await waveTrx.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    waaveTrx = await waveContract.connect(randomPerson).wave();
+    await waveTrx.wait();
+
+    waveCount = await waveContract.getTotalWaves();
   };
 
 const runMain = async() => {
